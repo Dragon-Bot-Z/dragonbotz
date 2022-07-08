fe449fda0553f74e410718c3a5fe9bd39ba61211
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-07-08 12:05:39 +0200

    Adds docstring

--- src/core/command.rs
+++ src/core/command.rs
@@ -9,6 +9,11 @@ use serenity::model::interactions::Interaction;
 #[async_trait]
 pub trait Command {
 
+    /// Executes the command's process
+    /// 
+    /// ## Arguments:
+    /// * context - the command's context
+    /// * interaction - the interaction that had triggered the command
     async fn run(_context: &Context, _interaction: &Interaction);
 
 }
