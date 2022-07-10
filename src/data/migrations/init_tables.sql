1e8487ab099b1c89ae7e4885202c3d4bdc915734
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-07-10 15:40:46 +0200

    Adds character table and populate script

--- src/data/migrations/init_tables.sql
+++ src/data/migrations/init_tables.sql
@@ -2,3 +2,11 @@ CREATE TABLE IF NOT EXISTS player(
     discord_id BIGINT PRIMARY KEY,
     register_date DATE DEFAULT NULL
 );
+
+CREATE TABLE IF NOT EXISTS character(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    rarity SMALLINT DEFAULT 0,
+    image TEXT DEFAULT 'https://i.imgur.com/eMiHxeP.png',
+    thumbnail TEXT DEFAULT 'https://i.imgur.com/eMiHxeP.png'
+);
--- src/data/migrations/populate.sql
+++ src/data/migrations/populate.sql
@@ -1,1 +1,6 @@
+-- inserting test values for player table
 INSERT INTO player VALUES(136160890000113664, '2022-07-09');
+
+-- inserting test values for character table
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus', 5, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
