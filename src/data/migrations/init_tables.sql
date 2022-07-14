30c9a2395011ce784fb226f83452bc6f5a8d72d2
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-07-14 14:15:36 +0200

    Adds banner, banner content tables and adds check contraint to character rarity

--- src/data/migrations/init_tables.sql
+++ src/data/migrations/init_tables.sql
@@ -1,3 +1,9 @@
+DROP TABLE player CASCADE;
+DROP TABLE character CASCADE;
+DROP TABLE banner CASCADE;
+DROP TABLE banner_content CASCADE;
+
+
 CREATE TABLE IF NOT EXISTS player(
     discord_id BIGINT PRIMARY KEY,
     register_date DATE DEFAULT NULL
@@ -8,5 +14,19 @@ CREATE TABLE IF NOT EXISTS character(
     name VARCHAR(50) UNIQUE NOT NULL,
     rarity SMALLINT DEFAULT 0,
     image TEXT DEFAULT 'https://i.imgur.com/eMiHxeP.png',
-    thumbnail TEXT DEFAULT 'https://i.imgur.com/eMiHxeP.png'
+    thumbnail TEXT DEFAULT 'https://i.imgur.com/eMiHxeP.png',
+    
+    CHECK (rarity >= 0 AND rarity <= 5)
+);
+
+CREATE TABLE IF NOT EXISTS banner(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS banner_content(
+    character INTEGER REFERENCES character,
+    banner INTEGER REFERENCES banner,
+
+    UNIQUE (character, banner)
 );
--- src/data/migrations/populate.sql
+++ src/data/migrations/populate.sql
@@ -4,3 +4,30 @@ INSERT INTO player VALUES(136160890000113664, '2022-07-09');
 -- inserting test values for character table
 INSERT INTO character(name, rarity, image, thumbnail) 
     VALUES('Thanus', 5, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
+
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus_ultra', 4, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
+
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus_extreme', 3, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
+
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus_super', 2, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');    
+
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus_uncommon', 1, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
+
+INSERT INTO character(name, rarity, image, thumbnail) 
+    VALUES('Thanus_common', 0, 'https://i.imgur.com/hwsOe06.png', 'https://i.imgur.com/QC3aaUU.png');
+
+-- inserting test values for banner table
+INSERT INTO banner VALUES(DEFAULT, 'Test Banner');
+
+-- inserting test values for banner content
+-- test banner
+INSERT INTO banner_content VALUES(1, 1);
+INSERT INTO banner_content VALUES(2, 1);
+INSERT INTO banner_content VALUES(3, 1);
+INSERT INTO banner_content VALUES(3, 1);
+INSERT INTO banner_content VALUES(4, 1);
+INSERT INTO banner_content VALUES(5, 1);
