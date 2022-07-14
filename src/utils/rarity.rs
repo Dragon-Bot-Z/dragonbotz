3b73a29e6e19f7eab5cbffd2a187a99432c0355b
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-07-14 17:26:58 +0200

    Fixes Extreme displaying

--- src/utils/rarity.rs
+++ src/utils/rarity.rs
@@ -14,7 +14,7 @@ impl std::fmt::Display for Rarity {
             Rarity::COMMON => "<:rarity_common:996897790334599198> **Common**",
             Rarity::UNCOMMON => "<:rarity_uncommon:996897775348351057> **Uncommon**",
             Rarity::SUPER => "<:rarity_super:996897783225270323> **Super**",
-            Rarity::EXTREME => "<:rarity_extreme:996897788543631370> **Extreme",
+            Rarity::EXTREME => "<:rarity_extreme:996897788543631370> **Extreme**",
             Rarity::ULTRA => "<:rarity_ultra:996897780314419291> **Ultra**",
             Rarity::KAMI => "<:rarity_kami:996897785532141618> **Kami**",
         };
